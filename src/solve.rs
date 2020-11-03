376f2944f96dd5d465c68c30bb7451c6e2d0ed1c
yozu <yasuo@ozu.email>
2020-11-03 22:57:16 +0900

    add iteration

--- src/solve.rs
+++ src/solve.rs
@@ -16,6 +16,7 @@ where
 {
 	model: &'a CompiledModel<Tp, Tq, Tc>,
 	qubits: Vec<&'a Qubit<Tq>>,
+	pub iterations: usize,
 	pub samples: usize,
 	// pub processes: usize,
 	pub generations: usize,
@@ -37,6 +38,7 @@ where
 			qubits,
 			samples: 8, // 8
 			// processes: 1,
+			iterations: 10,
 			generations: 30,     // 10
 			beta_count: 100,     // 100
 			sweeps_per_beta: 30, // 30
@@ -90,85 +92,86 @@ where
 {
 	/// Solve the model using internal annealer.
 	pub fn solve(&self) -> (f64, HashMap<&Tq, bool>, Vec<&Tc>) {
-		let mut phdict: HashMap<&Placeholder<Tp, Tc>, usize> = self
-			.model
-			.get_placeholders()
-			.into_iter()
-			.map(|p| (p, 1))
-			.collect();
-		let mut size = phdict.len();
+		let ph = self.model.get_placeholders();
 		let mut ret = None;
-		for _ in 0..self.generations {
-			let (c, h, neighbors) = self.model.generate_qubo(&self.qubits, &mut |p| {
-				if let Some(cnt) = phdict.get(&p) {
-					*cnt as f64 / size as f64 * self.coeff_strength
-				} else {
-					panic!()
-				}
-			});
-			let neighbors = neighbors
-				.iter()
-				.map(|v| v.deref())
-				.collect::<Vec<&[(usize, f64)]>>();
-			let (beta_min, beta_max) = Self::generate_beta_range(&h, &neighbors);
-			let beta_schedule = Self::generate_beta_schedule(beta_min, beta_max, self.beta_count);
-			let fut_ret = std::iter::repeat((&h, &neighbors))
-				.take(self.samples)
-				.collect::<Vec<_>>()
-				.par_iter()
-				.map(|(h, neighbors)| {
-					let annealer = SimpleAnnealer::new(self.sweeps_per_beta, beta_schedule.clone());
-					let mut r = SmallRng::from_rng(OsRng).unwrap();
-					let mut state = QubitState::new_random(self.qubits.len(), &mut r);
-					annealer.run(&mut state, &mut r, &h, &neighbors);
-					(Self::calculate_energy(&mut state, c, &h, &neighbors), state)
-				})
-				.collect::<Vec<_>>();
-			let max = fut_ret.iter().fold(0.0 / 0.0, |m, v| v.0.max(m));
-			let (energy, state) = fut_ret
-				.into_iter()
-				.filter(|(e, _)| *e == max)
-				.next()
-				.unwrap();
-			if let Some((old_energy, _, _)) = ret {
+		for _ in 0..self.iterations {
+			let mut phdict: HashMap<&Placeholder<Tp, Tc>, usize> =
+				ph.iter().map(|p| (*p, 10)).collect();
+			let mut size = ph.len() * 10;
+			let mut old_energy = f64::INFINITY;
+			for _ in 0..self.generations {
+				let (c, h, neighbors) = self.model.generate_qubo(&self.qubits, &mut |p| {
+					if let Some(cnt) = phdict.get(&p) {
+						*cnt as f64 / size as f64 * self.coeff_strength
+					} else {
+						panic!()
+					}
+				});
+				let neighbors = neighbors
+					.iter()
+					.map(|v| v.deref())
+					.collect::<Vec<&[(usize, f64)]>>();
+				let (beta_min, beta_max) = Self::generate_beta_range(&h, &neighbors);
+				let beta_schedule =
+					Self::generate_beta_schedule(beta_min, beta_max, self.beta_count);
+				let fut_ret = std::iter::repeat((&h, &neighbors))
+					.take(self.samples)
+					.collect::<Vec<_>>()
+					.par_iter()
+					.map(|(h, neighbors)| {
+						let annealer =
+							SimpleAnnealer::new(self.sweeps_per_beta, beta_schedule.clone());
+						let mut r = SmallRng::from_rng(OsRng).unwrap();
+						let mut state = QubitState::new_random(self.qubits.len(), &mut r);
+						annealer.run(&mut state, &mut r, &h, &neighbors);
+						(Self::calculate_energy(&mut state, c, &h, &neighbors), state)
+					})
+					.collect::<Vec<_>>();
+				let max = fut_ret.iter().fold(0.0 / 0.0, |m, v| v.0.max(m));
+				let (energy, state) = fut_ret
+					.into_iter()
+					.filter(|(e, _)| *e == max)
+					.next()
+					.unwrap();
 				if old_energy < energy {
 					continue;
 				}
-			}
-			let ans: HashMap<&Qubit<Tq>, bool> = self
-				.qubits
-				.iter()
-				.enumerate()
-				.map(|(i, q)| (*q, state.get(i)))
-				.collect();
-			let mut constraint_labels = Vec::new();
-			for c in self.model.get_unsatisfied_constraints(&ans) {
-				if let Some(ph) = &c.placeholder {
-					if let Some(point) = phdict.get_mut(ph) {
-						*point += 1;
-						size += 1;
+				old_energy = energy;
+				let ans: HashMap<&Qubit<Tq>, bool> = self
+					.qubits
+					.iter()
+					.enumerate()
+					.map(|(i, q)| (*q, state.get(i)))
+					.collect();
+				let mut constraint_labels = Vec::new();
+				for c in self.model.get_unsatisfied_constraints(&ans) {
+					if let Some(ph) = &c.placeholder {
+						if let Some(point) = phdict.get_mut(ph) {
+							*point += 1;
+							size += 1;
+						}
+					}
+					if let Some(label) = &c.label {
+						constraint_labels.push(label);
 					}
 				}
-				if let Some(label) = &c.label {
-					constraint_labels.push(label);
+				let is_satisfied = constraint_labels.len() == 0;
+				ret = Some((
+					energy,
+					ans.into_iter()
+						.filter_map(|(q, b)| {
+							if let Qubit::Qubit(q) = q {
+								Some((q, b))
+							} else {
+								None
+							}
+						})
+						.collect(),
+					constraint_labels,
+				));
+				if is_satisfied {
+					return ret.unwrap();
 				}
-			}
-			let is_satisfied = constraint_labels.len() == 0;
-			ret = Some((
-				energy,
-				ans.into_iter()
-					.filter_map(|(q, b)| {
-						if let Qubit::Qubit(q) = q {
-							Some((q, b))
-						} else {
-							None
-						}
-					})
-					.collect(),
-				constraint_labels,
-			));
-			if is_satisfied {
-				break;
 			}
 		}
 		ret.unwrap()
